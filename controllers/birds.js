dce2a17af263ef3a379ff9582765601d41a7be87
Chase <cabbott93@gmail.com>
2021-06-11 09:10:27 -0700

    TDD bird routes

--- controllers/birds.js
+++ controllers/birds.js
@@ -1,7 +1,39 @@
 const { Router } = require('express');
+const Bird = require('../models/Bird');
 
 module.exports = Router()
-  .post()
-  .get()
-  .put()
-  .delete();
+  .post('/api/v1/birds', async (req, res) => {
+    try {
+      const bird = await Bird.insertBird(req.body);
+      res.send(bird);
+    }
+    catch(err){
+      res.status(500).send(err);
+    }
+  })
+  .get('/api/v1/birds', async (req, res) => {
+    try {
+      const birds = await Bird.selectBirds();
+      res.send(birds);
+    }
+    catch(err) {
+      res.status(500).send(err);
+    }})
+  .put('/api/v1/birds/:id', async (req, res) => {
+    try {
+      const bird = await Bird.updateBird(req.params.id, req.body);
+      res.send(bird);
+    }
+    catch(err){
+      res.status(500).send(err);
+    }
+  })
+  .delete('/api/v1/birds/:id', async (req, res) => {
+    try {
+      const bird = await Bird.deleteBird(req.params.id);
+      res.send(bird);
+    }
+    catch(err){
+      res.status(500).send(err);
+    }
+  });
